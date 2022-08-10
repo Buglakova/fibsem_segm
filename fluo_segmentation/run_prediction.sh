eaa71df15317c8c27115333853c96fb56a1ccc03
Buglakova <elena.v.buglakova@gmail.com>
2022-08-10 14:10:42 +0200

    Make a script for fluorescence nuclei prediction.

---
+++ fluo_segmentation/run_prediction.sh
@@ -0,0 +1,2 @@
+export BIOIMAGEIO_CACHE_PATH="/g/kreshuk/buglakova/biomodel_zoo_tmp/"
+CUDA_VISIBLE_DEVICES=4 python adrian_unet_predict.py /g/kreshuk/buglakova/data/cryofib/registration_fluo/F059/F059_fluo.n5/ "raw" /g/kreshuk/buglakova/data/cryofib/registration_fluo/F059/F059_fluo.n5/ "segmentation/adrian_unet"
