f8694f7fa83c224c06843c009872953f91a4baa8
Buglakova <elena.v.buglakova@gmail.com>
2022-08-10 14:09:24 +0200

    Convert F059 from tif to n5.

--- cryofib/n5_utils.py
+++ cryofib/n5_utils.py
@@ -1,5 +1,5 @@
 import z5py
-from pathlib import Path
+from pathlib import Path, PurePath
 import numpy as np
 from typing import Tuple, Dict
 from elf.io import open_file
@@ -11,6 +11,10 @@ def print_key_tree(f: z5py.File):
 
 
 def read_volume(f: z5py.File, key: str, roi: np.lib.index_tricks.IndexExpression = np.s_[:]):
+    print(type(f))
+    if isinstance(f, (str, PurePath)):
+        f = z5py.File(f, "a")
+
     try:
         ds = f[key]
     except KeyError:
@@ -24,6 +28,20 @@ def read_volume(f: z5py.File, key: str, roi: np.lib.index_tricks.IndexExpression
     print(f"Read volume with shape {vol.shape}, data type {vol.dtype}")
     
     return vol
+
+def get_attrs(f: z5py.File, key: str):
+    print(type(f))
+    if isinstance(f, (str, PurePath)):
+        f = z5py.File(f, "a")
+
+    try:
+        ds = f[key]
+    except KeyError:
+        print(f"No key {key} in file {f.filename}")
+        print_key_tree(f)
+        return None
+    
+    return ds.attrs
 
 
 def create_ind(arr: np.array):
@@ -45,10 +63,14 @@ def write_ind(ind_exp: np.lib.index_tricks.IndexExpression):
     return start, stop, step
 
 
-def write_volume(f, arr: np.array, key, chunks=(1, 512, 512)):
+def write_volume(f, arr: np.array, key, chunks=(1, 512, 512), attrs=None):
     shape = arr.shape
     compression = "gzip"
     dtype = arr.dtype
+
+    print(type(f))
+    if isinstance(f, (str, PurePath)):
+        f = z5py.File(f, "a")
 
     if key not in f.keys():
         print(f"Created dataset {key}")
@@ -62,6 +84,11 @@ def write_volume(f, arr: np.array, key, chunks=(1, 512, 512)):
     print(f"Writing array to {key}")
     ds[:] = arr
 
+    if attrs is not None:
+        print(f"Assigning attributes of the dataset")
+        for key in attrs.keys():
+            ds.attrs[key] = attrs[key]
+
 
 def tif2n5(tif_dir: Path,
             n5_path: Path,
@@ -73,7 +100,7 @@ def tif2n5(tif_dir: Path,
             resolution: Tuple = (1, 1, 1),
             roi: Dict = None):
 
-    f_out = z5py.File(n5_path, "w")
+    f_out = z5py.File(n5_path, "a")
     with open_file(str(tif_dir)) as f:
         print(f"Reading tif files from {tif_dir}")
         tiff_stack_obj = f[reg_exp]
@@ -85,12 +112,19 @@ def tif2n5(tif_dir: Path,
         compression = "gzip"
         dtype = imgs.dtype
 
-        ds = f_out.create_dataset(n5_key, shape=shape, compression=compression,
-                            chunks=chunks, dtype=dtype, n_threads=8)
-        print(f"Writing to {n5_path}, key {n5_key}")
+        if n5_key not in f_out.keys():
+            print(f"Created dataset {n5_key}")
+            ds = f_out.create_dataset(n5_key, shape=shape, compression=compression,
+                                chunks=chunks, dtype=dtype, n_threads=8)
+
+        else:
+            print(f"Overwriting {n5_key}")
+            ds = f_out[n5_key]
+
+        print(f"Writing to {n5_path}, n5_key {n5_key}")
         ds[:] = imgs
 
-        # Attributes required for future registration
+        # Attributes required for future registration/viewing
         print(f"Assigning attributes of the dataset")
         attributes = ds.attrs
         attributes["description"] = description
@@ -100,3 +134,5 @@ def tif2n5(tif_dir: Path,
             start, stop, step = create_ind(imgs)
             roi = {"start": start, "stop": stop, "step": step}
         attributes['roi'] = roi
+
+    return imgs, attributes
---
+++ data_management/convert_to_n5_F059.py
@@ -0,0 +1,93 @@
+import numpy as np
+from elf.io import open_file
+import z5py
+from pathlib import Path
+
+
+from cryofib.n5_utils import read_volume, tif2n5, create_ind, write_volume
+
+def rotate_x_90(array):
+        """
+        Edoardo:
+        To register the FIBSEM data I first flip it horizontally
+        and then invert the axes as XYZ —> YZX (not clear yet why Dragonfly does it this way)
+        to achieve a 90° rotation around the x axis to match the fluorescence orientation
+        """
+        assert array.ndim == 3
+        return np.moveaxis(np.flip(array, axis=2), [0, 1, 2], [2, 0, 1])
+
+
+
+
+def main():
+        data_dir = Path("/g/kreshuk/buglakova/data/cryofib/raw/F059")
+        fluo_path = data_dir / "fluo" / "After-FS_Block_06_BT474_H_SirActin_organoid01_branding_confocal_from_surface_slow-white-nuclei_only.tif"
+        em_path = data_dir / "em" / "F059_A1_raw"
+        em_segm_nuclei_path = data_dir / "em" / "F059_A1_segm_nuclei"
+
+        registration_dir = Path("/g/kreshuk/buglakova/data/cryofib/registration_fluo/F059")
+        segmentation_dir = Path("/g/kreshuk/buglakova/data/cryofib/segm_fibsem/F059")
+        
+        # Convert fluorescent data to n5
+        tif2n5(tif_dir=fluo_path,
+                n5_path=registration_dir / "F059_fluo.n5",
+                n5_key="raw",
+                reg_exp="",
+                description="Raw fluorescence data F059",
+                order="zyx",
+                unit="nm",
+                resolution=(2869.8, 332.1, 332.1),
+                roi=None)
+
+
+        # Convert EM data for segmentation
+        em_raw, em_raw_attrs = tif2n5(tif_dir=em_path,
+                n5_path=segmentation_dir / "F059_em.n5",
+                n5_key="raw",
+                reg_exp="*.tif",
+                description="Raw FIB-SEM data F059_A1",
+                order="zyx",
+                unit="nm",
+                resolution=(20, 20, 20),
+                roi=None)
+
+        em_segm_nuclei, em_segm_nuclei_attrs = tif2n5(tif_dir=em_segm_nuclei_path,
+                n5_path=segmentation_dir / "F059_A1_em.n5",
+                n5_key="segmentation/nuclei",
+                reg_exp="*.tiff",
+                description="Nuclei segmentation done by Edoardo in Dragonfly",
+                order="zyx",
+                unit="nm",
+                resolution=(20, 20, 20),
+                roi=None)
+
+        # For registration the volume has to be rotated
+        # Edoardo:
+        # To register the FIBSEM data I first flip it horizontally
+        # and then invert the axes as XYZ —> YZX (not clear yet why Dragonfly does it this way)
+        # to achieve a 90° rotation around the x axis to match the fluorescence orientation
+
+        em_raw_rotated = rotate_x_90(em_raw)
+        em_segm_nuclei_rotated = rotate_x_90(em_segm_nuclei)
+        
+        em_raw_attrs["order"] = "xzy"
+        em_segm_nuclei_attrs["order"] = "xzy"
+
+        start, stop, step = create_ind(em_raw_rotated)
+        roi = {"start": start, "stop": stop, "step": step}
+        em_raw_attrs['roi'] = roi
+        em_segm_nuclei_attrs['roi'] = roi
+
+        write_volume(registration_dir / "F059_A1_em.n5",
+                                em_raw_rotated,
+                                "raw",
+                                attrs=em_raw_attrs)
+
+        write_volume(registration_dir / "F059_A1_em.n5",
+                        em_segm_nuclei_rotated,
+                        "segmentation/nuclei",
+                        attrs=em_segm_nuclei_attrs)
+
+
+if __name__ == "__main__":
+    main()
