d9e08726ea5efe7725bc87ee4f2841e79bf8d4d6
Buglakova <elena.v.buglakova@gmail.com>
2022-12-23 17:27:51 +0100

    Move registration to another repo.

--- data_management/pipelines/registration_fluo_F059_A1.sh
+++
@@ -1,6 +0,0 @@
-python convert_to_n5_F059.py
-
-conda activate biomodel_env
-export BIOIMAGEIO_CACHE_PATH="/g/kreshuk/buglakova/biomodel_zoo_tmp/"
-CUDA_VISIBLE_DEVICES=4 python adrian_unet_predict.py /g/kreshuk/buglakova/data/cryofib/registration_fluo/F059/F059_fluo.n5/ "raw" /g/kreshuk/buglakova/data/cryofib/registration_fluo/F059/F059_fluo.n5/ "segmentation/adrian_unet"
-
--- fluo_segmentation/run_prediction_F107.sh
+++
@@ -1,2 +0,0 @@
-export BIOIMAGEIO_CACHE_PATH="/g/kreshuk/buglakova/biomodel_zoo_tmp/"
-CUDA_VISIBLE_DEVICES=4 python adrian_unet_predict.py /g/kreshuk/buglakova/data/cryofib/registration_fluo/F107/F107_fluo.n5/ "raw" /g/kreshuk/buglakova/data/cryofib/registration_fluo/F107/F107_fluo.n5/ "segmentation/adrian_unet"
