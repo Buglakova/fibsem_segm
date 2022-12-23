5dc88cd50df8d0d05e0ac61632d16299e1546388
Buglakova <elena.v.buglakova@gmail.com>
2022-12-23 17:26:36 +0100

    Ignore resolution if not found when loading group.

--- cryofib/napari_utils.py
+++ cryofib/napari_utils.py
@@ -13,7 +13,11 @@ def add_group_layer(name, obj, viewer=None, roi=np.s_[:]):
     assert viewer is not None 
     if isinstance(obj, z5py.dataset.Dataset):
         data = obj[roi]
-        if np.issubdtype(data.dtype, np.integer):
-            viewer.add_labels(data, name=name, scale=obj.attrs["resolution"])
+        if "resolution" in obj.attrs.keys():
+            resolution = obj.attrs["resolution"]
         else:
-            viewer.add_image(data, name=name, scale=obj.attrs["resolution"])
+            resolution = np.ones_like(data.shape)
+        if np.issubdtype(data.dtype, np.integer):
+            viewer.add_labels(data, name=name, scale=resolution)
+        else:
+            viewer.add_image(data, name=name, scale=resolution)
